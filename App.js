18e0fcc5125f29968d567c95e6ab3c207df7a76b
Andrew McGrath <27497493+am283721@users.noreply.github.com>
2019-03-15 15:03:26 -0400

    Styling Updates

--- App.js
+++ App.js
@@ -1,80 +1,95 @@
 Ext.define('Rally.newProject.StoryGrid', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    launch: function() {
-        // Add iteration combobox to the page
+    launch: function () {
+        // Add iteration combobox and print button to the page
         this.add({
-            xtype: 'rallyiterationcombobox',
-            id: 'iterationComboBox',
-            listeners: {
-                ready: this._onIterationsLoaded,
-                select: this._onIterationChanged,
-                scope: this
-            }
+            xtype: 'container',
+            flex: 1,
+            width: '800px',
+            layout: {
+                type: 'hbox',
+                defaultMargins: '25 50 25 50'
+            },
+            items: [
+                {
+                    xtype: 'rallyiterationcombobox',
+                    id: 'iterationComboBox',
+                    listeners: {
+                        ready: this._onIterationsLoaded,
+                        select: this._onIterationChanged,
+                        scope: this
+                    },
+                    fieldLabel: 'Select an Iteration ',
+                    width: '350px'
+                },
+                {
+                    xtype: 'button',
+                    text: 'Print to PDF',
+                    id: 'printPdfBtn',
+                    handler: () => {
+                        printJS({
+                            printable: 'userStoryGrid',
+                            type: 'html',
+                            // css files need to be passed along as grid doesn't display properly otherwise
+                            css: [
+                                '/apps/2.0/rui/resources/css/rui-all_01.css',
+                                '/apps/2.0/rui/resources/css/rui-all_02.css',
+                                '/apps/2.0/rui/resources/css/rui-fonts.css'
+                            ],
+                            scanStyles: false, // Disable scanning of styles since we're passing the styles in the function call
+                            documentTitle: 'User Stories for Iteration: ' + this.down('rallyiterationcombobox').rawValue
+                        });
+                    }
+                }
+            ]
         });
     },
 
-    _onIterationsLoaded: function() {
+    _onIterationsLoaded: function () {
         // Add the user story grid to the page
         this.add({
             xtype: 'rallygrid',
             id: 'userStoryGrid',
             columnCfgs: [
-            {
-                text: 'Story Name',
-                dataIndex: 'Name',
-                width: 350
-            },
-            {
-                text: 'Schedule State',
-                dataIndex: 'ScheduleState',
-                width: 150
-            },
-            {
-                text: 'Part of Awesome Town?',
-                dataIndex: 'AwesomeTown',
-                width: 200
-            }
+                {
+                    text: 'Story Name',
+                    dataIndex: 'Name',
+                    width: 400
+                },
+                {
+                    text: 'Schedule State',
+                    dataIndex: 'ScheduleState',
+                    renderer: function (value) {
+                        return value;
+                    },
+                    width: 200
+                },
+                {
+                    text: 'Part of Awesome Town?',
+                    dataIndex: 'AwesomeTown',
+                    width: 200
+                }
             ],
             context: this.getContext(),
             enableEditing: false,
             showPagingToolbar: false,
+            showRowActionsColumn: false,
             storeConfig: {
                 model: 'userstory',
                 filters: [this._getIterationFilter()]
             }
         });
-        
-        // Add the 'Print to PDF' button to the page
-        this.add({
-            xtype: 'button',
-            text: 'Print to PDF',
-            id: 'printPdfBtn',
-            handler: () => {
-                printJS({
-                    printable: 'userStoryGrid', 
-                    type: 'html',
-                    // css files need to be passed along as grid doesn't display properly otherwise
-                    css: [
-                        '/apps/2.0/rui/resources/css/rui-all_01.css', 
-                        '/apps/2.0/rui/resources/css/rui-all_02.css', 
-                        '/apps/2.0/rui/resources/css/rui-fonts.css'
-                    ],
-                    scanStyles: false, // Disable scanning of styles since we're passing the styles in the function call
-                    documentTitle: 'User Stories for Iteration: ' + this.down('rallyiterationcombobox').rawValue
-                });
-            }
-        });
     },
 
-    _onIterationChanged: function() {
+    _onIterationChanged: function () {
         let storyGrid = this.down('rallygrid'),
             storyStore = storyGrid.getStore();
-            storyStore.clearFilter(true);
-            storyStore.filter(this._getIterationFilter());
+        storyStore.clearFilter(true);
+        storyStore.filter(this._getIterationFilter());
     },
 
-    _getIterationFilter: function() {
+    _getIterationFilter: function () {
         return this.down('rallyiterationcombobox').getQueryFromSelected();
     },
 });
--- README.md
+++ README.md
@@ -11,6 +11,6 @@ This sample app uses the [printjs library](https://github.com/crabbly/Print.js).
 
 rally-grid-to-pdf is released under the MIT license.  See the file [LICENSE](./LICENSE) for the full text.
 
-##Documentation for SDK
+## Documentation for SDK
 
 You can find the documentation on our help [site.](https://help.rallydev.com/apps/2.1/doc/)
